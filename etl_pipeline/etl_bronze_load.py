e6b5b5517e107665cf8f742c2edf78801537c110
astankie112358 <astankie@wp.pl>
2025-12-04 21:55:20 +0000

    fix packaged

--- etl_pipeline/etl_bronze_load.py
+++ etl_pipeline/etl_bronze_load.py
@@ -1,13 +1,11 @@
 from pyspark import pipelines as dp
-from pyspark.sql.functions import col
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 from pyspark.sql.types import StructType, StructField, StringType
 
-catalog="nba"
-volume_name="games"
+catalog = "nba"
+volume_name = "games"
 bronze_schema = "bronze"
 source_schema = "source"
-table_name="games"
+table_name = "games"
 
 game_schema = StructType([
     StructField("game_id", StringType(), True),
@@ -16,10 +14,10 @@ game_schema = StructType([
     StructField("date", StringType(), True)
 ])
 
-@dlt.table(name=f"{catalog}.{bronze_schema}.{table_name}")
+@dp.table(name=f"{catalog}.{bronze_schema}.{table_name}")
 def load_games():
-   df = (
-      spark.readStream
+    return (
+        spark.readStream
         .format("cloudFiles")
         .option("cloudFiles.format", "parquet")
         .option("header", "true")
@@ -27,4 +25,3 @@ def load_games():
         .schema(game_schema)
         .load(f"/Volumes/{catalog}/{source_schema}/{volume_name}")
     )
-   return df
