373a2e0b9b3af7730443bda3f89ea56702085fcc
astankie112358 <astankie@wp.pl>
2025-12-15 19:33:05 +0000

    add first gold

--- etl_pipeline/etl_gold_load.py
+++ etl_pipeline/etl_gold_load.py
@@ -1,4 +1,5 @@
 from pyspark import pipelines as dp
+from pyspark.sql.functions import col, lit
 
 catalog = "nba"
 silver_schema = "silver"
@@ -12,9 +13,24 @@ player_stats_table_name = "game_player_stats"
 
 @dp.table(name=f"{catalog}.{gold_schema}.{boxscore_table_name}")
 def games():
-  return spark.read.table(f"{catalog}.{silver_schema}.{boxscore_table_name}")\
-          .melt(
-            id_vars=[
+  df = spark.read.table(f"{catalog}.{silver_schema}.{boxscore_table_name}")
+  home_df = df.select(
+    "game_id", "date", "regulation_time",
+    col("home_team_id").alias("team_id"),
+    lit("home").alias("team_type"),
+    col("home_team_result").alias("team_result"),
+    col("away_team_id").alias("opponent_id"),
+    col("away_team_result").alias("opponent_result")
+  )
+  away_df = df.select(
+      "game_id", "date", "regulation_time",
+      col("away_team_id").alias("team_id"),
+      lit("away").alias("team_type"),
+      col("away_team_result").alias("team_result"),
+      col("home_team_id").alias("opponent_id"),
+      col("home_team_result").alias("opponent_result")
+  )
+  return home_df.unionByName(away_df)
   
 
 
